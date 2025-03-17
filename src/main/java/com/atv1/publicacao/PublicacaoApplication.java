bb4669bebb36e2a7d14cfa68d7ae9a758aae1ea7
lucasbomfim15 <bmfmlucas@gmail.com>
2025-03-17 15:02:26 -0300

    first commit

---
+++ src/main/java/com/atv1/publicacao/PublicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.atv1.publicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicacaoApplication.class, args);
+	}
+
+}
