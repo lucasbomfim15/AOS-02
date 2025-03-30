c6564bd9be56b97d5f4ffc1f05cf1a16280a84f0
lucasbomfim15 <bmfmlucas@gmail.com>
2025-03-30 00:00:46 -0300

    modified 2

--- src/main/java/com/atv1/publicacao/ConfigController.java
+++ src/main/java/com/atv1/publicacao/ConfigController.java
@@ -1,4 +1,21 @@
 package com.atv1.publicacao;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/config")
 public class ConfigController {
+
+    @Value("${backend.url}")
+    private String backendUrl;
+
+    @GetMapping
+    public Map<String, String> getConfig() {
+        return Map.of("backendUrl", backendUrl);
+    }
 }
